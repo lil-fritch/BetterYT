c708fc87707b01d1b371880c224df91db535b3c7
andelgg <andolanderfgh@gmail.com>
2024-12-28 20:09:55 +0300

    popup

--- src/window.js
+++ src/window.js
@@ -1,1 +1,11 @@
 document.addEventListener('contextmenu', (event) => event.preventDefault());
+
+const toggleSwitch = document.getElementById('toggleSwitch');
+
+toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+        console.log('Switched to: Hide');
+    } else {
+        console.log('Switched to: Remove');
+    }
+});
